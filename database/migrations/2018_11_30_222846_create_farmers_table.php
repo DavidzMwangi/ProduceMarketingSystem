8cd3b8e1ab2d3a890424aedeb140de2d00cae9a9
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2018-12-01 01:38:40 +0300

    completed database

---
+++ database/migrations/2018_11_30_222846_create_farmers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFarmersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('farmers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('crop_id');
+            $table->string('phone_number');
+            $table->string('email')->nullable();
+            $table->timestamps();
+
+            $table->foreign('crop_id')->references('id')->on('crops')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('farmers');
+    }
+}
---
+++ database/migrations/2018_11_30_223214_create_locations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('farmer_id');
+            $table->string('long');
+            $table->string('lat');
+            $table->timestamps();
+
+            $table->foreign('farmer_id')->references('id')->on('farmers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
